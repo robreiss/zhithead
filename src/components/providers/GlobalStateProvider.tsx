433b53f466e996487b8b7947991ace762727d2cc
Rob Reiss <3080274+robreiss@users.noreply.github.com>
2024-10-10 09:05:02 -0700

    Add ability to view service in devtools via global

--- src/components/providers/GlobalStateProvider.tsx
+++ src/components/providers/GlobalStateProvider.tsx
@@ -1,14 +1,31 @@
-import { createContext, PropsWithChildren } from "react";
 import { useInterpret } from "@xstate/react";
-import { zhitheadMachine } from "../../state/machines/zhithead.machine";
+import { createContext, PropsWithChildren, useEffect } from "react";
 import { InterpreterFrom } from "xstate";
+import { zhitheadMachine } from "../../state/machines/zhithead.machine";
 
 export const GlobalStateContext = createContext({
   zhitheadService: {} as InterpreterFrom<typeof zhitheadMachine>,
 });
 
+// Add this type declaration to extend the Window interface
+declare global {
+  interface Window {
+    zhitheadService?: InterpreterFrom<typeof zhitheadMachine>;
+  }
+}
+
 export default function GlobalStateProvider(props: PropsWithChildren) {
   const zhitheadService = useInterpret(zhitheadMachine);
+
+  useEffect(() => {
+    // Attach zhitheadService to the window object
+    window.zhitheadService = zhitheadService;
+
+    // Cleanup function to remove the global variable when the component unmounts
+    return () => {
+      delete window.zhitheadService;
+    };
+  }, [zhitheadService]);
 
   return (
     <GlobalStateContext.Provider value={{ zhitheadService }}>
